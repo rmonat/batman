a847facb63ea5d7b43695f995c417aa56d77a47d
Raphaël Monat <raphael.monat@ens-lyon.org>
2017-01-20 22:27:43 +0100

    small update

--- Makefile
+++ Makefile
@@ -20,7 +20,7 @@
 all: analyzer
 
 analyzer : 
-	ocamlbuild -tag custom -use-menhir batman.byte -use-ocamlfind -package apron,apron.apron,apron.boxD,apron.boxMPFR,apron.boxMPQ,apron.octD,apron.octMPQ,apron.polkaGrid,apron.polkaMPQ,apron.polkaRll,apron.ppl,apron.t1pD,apron.t1pMPFR,apron.t1pMPQ,gmp,bddapron,bddapron.bdd,bddapron.bddapron
+	ocamlbuild -use-menhir batman.byte -use-ocamlfind -package apron,apron.apron,apron.boxD,apron.boxMPFR,apron.boxMPQ,apron.octD,apron.octMPQ,apron.polkaGrid,apron.polkaMPQ,apron.polkaRll,apron.ppl,apron.t1pD,apron.t1pMPFR,apron.t1pMPQ,gmp,bddapron,bddapron.bdd,bddapron.bddapron
 
 debug:
 	ocamlbuild -tag debug -use-menhir batman.d.byte -use-ocamlfind -package apron,apron.apron,apron.boxD,apron.boxMPFR,apron.boxMPQ,apron.octD,apron.octMPQ,apron.polkaGrid,apron.polkaMPQ,apron.polkaRll,apron.ppl,apron.t1pD,apron.t1pMPFR,apron.t1pMPQ,gmp,bddapron,bddapron.bdd,bddapron.bddapron
--- bddapron_domain.ml
+++ bddapron_domain.ml
@@ -29,9 +29,9 @@ module BDDAPRON_DOMAIN(Param: BDDAPRON_PARAM) =
   (struct
 
     let apron = Param.manager
-    let man = Mtbdddomain1.make_man apron
+    let man = Bdddomain1.make_man apron
 
-    type domain = (string, Param.lib) Bddapron.Mtbdddomain1.t
+    type domain = (string, Param.lib) Bddapron.Bdddomain1.t
     type env = string Bddapron.Env.t
     type apron_expr = string Bddapron.Expr1.Apron.t
     type bool_expr = string Bddapron.Expr1.Bool.t
@@ -53,10 +53,10 @@ module BDDAPRON_DOMAIN(Param: BDDAPRON_PARAM) =
 
 
     let mkenv () =  Env.make ~bddsize:1000 ~symbol:Env.string_symbol ~relational:true cudd
-    let empty () = Mtbdddomain1.top man (Env.make ~symbol:Env.string_symbol cudd)
+    let empty () = Bdddomain1.top man (Env.make ~symbol:Env.string_symbol cudd)
 
-    let top env = Mtbdddomain1.top man env
-    let bot env = Mtbdddomain1.bottom man env
+    let top env = Bdddomain1.top man env
+    let bot env = Bdddomain1.bottom man env
 
     let bint_cst env vmax v = Expr1.Bint.of_int env cond (`Bint (false, vmax)) v
     let bint_expr e = Expr1.Bint.to_expr e
@@ -91,30 +91,30 @@ module BDDAPRON_DOMAIN(Param: BDDAPRON_PARAM) =
     let env_print fmt e = Env.print fmt e
 
 
-    let eq a b = Mtbdddomain1.is_eq man a b
+    let eq a b = Bdddomain1.is_eq man a b
 
-    let subseteq a b = Mtbdddomain1.is_leq man a b
+    let subseteq a b = Bdddomain1.is_leq man a b
 
-    let print fmt a = Mtbdddomain1.print fmt a
+    let print fmt a = Bdddomain1.print fmt a
 
-    let getenv dom = Mtbdddomain1.get_env dom
+    let getenv dom = Bdddomain1.get_env dom
 
     let getcudd () = cudd
 
 
-    let join a b = Mtbdddomain1.join man a b
+    let join a b = Bdddomain1.join man a b
 
-    let meet a b = Mtbdddomain1.meet man a b
+    let meet a b = Bdddomain1.meet man a b
         
-    let widen a b = Mtbdddomain1.widening man a b
+    let widen a b = Bdddomain1.widening man a b
 
-    let widen_threshold a b threshold = Mtbdddomain1.widening_threshold man a b threshold
+    let widen_threshold a b threshold = Bdddomain1.widening_threshold man a b threshold
 
-    let assign dom var a = Mtbdddomain1.assign_lexpr ~relational:true man cond dom [var] [a] None, ()
+    let assign dom var a = Bdddomain1.assign_lexpr ~relational:true man cond dom [var] [a] None, ()
 
     let to_lincons_array dom = 
-      let domains = List.map (fun (x, y) -> y) (Mtbdddomain1.to_bddapron man dom) in
-      let d = List.fold_left (fun x y -> Apron.Abstract1.join apron x y) (Apron.Abstract1.bottom apron (Env.apron(Mtbdddomain1.get_env dom))) domains in
+      let domains = List.map (fun (x, y) -> y) (Bdddomain1.to_bddapron man dom) in
+      let d = List.fold_left (fun x y -> Apron.Abstract1.join apron x y) (Apron.Abstract1.bottom apron (Env.apron(Bdddomain1.get_env dom))) domains in
       Apron.Abstract1.to_lincons_array apron d
 
     let get_apron_env dom = 
@@ -122,13 +122,13 @@ module BDDAPRON_DOMAIN(Param: BDDAPRON_PARAM) =
       Env.apron env
     
     let filter dom b = 
-      Mtbdddomain1.meet_condition man cond dom b, Mtbdddomain1.meet_condition man cond dom (Expr1.Bool.dnot cond b), ()
+      Bdddomain1.meet_condition man cond dom b, Bdddomain1.meet_condition man cond dom (Expr1.Bool.dnot cond b), ()
     
 
-    let add_vars ?(init=true) (*?(init=true) on suppose que y'a jamais besoin d'initialiser*) dom vars = 
-      let env = Mtbdddomain1.get_env dom in
+    let add_vars ?(init=true) (*?(init=true) on suppose que y'a jamais besoin d'initialiser*) dom vars =
+      let env = Bdddomain1.get_env dom in
       let env = Env.add_vars env vars in
-      let dom = Mtbdddomain1.change_environment man dom env in
+      let dom = Bdddomain1.change_environment man dom env in
       if (init) then
         (
           let rec do_init dom remaining_vars = match remaining_vars with
@@ -145,16 +145,16 @@ module BDDAPRON_DOMAIN(Param: BDDAPRON_PARAM) =
           
 
     let del_vars (dom:domain) vars =
-      let env = Mtbdddomain1.get_env dom in
+      let env = Bdddomain1.get_env dom in
       (*Bddapron.Env.print Format.std_formatter env;*)
       let env = Env.remove_vars env (List.map (fun (x, y) -> x) vars) in
-      Mtbdddomain1.change_environment man dom env
+      Bdddomain1.change_environment man dom env
 
     let ren_vars (dom:domain) begv endv = 
-(*      let env = Mtbdddomain1.get_env domain in
+(*      let env = Bdddomain1.get_env domain in
       let env = Env.rename_vars env (List.map2 (fun x y -> (x, y)) begv endv) in
       Mbtdddomain1.change_environment man domain env*)
-      Mtbdddomain1.rename man dom (List.map2 (fun x y -> (fst x, fst y)) begv endv)
+      Bdddomain1.rename man dom (List.map2 (fun x y -> (fst x, fst y)) begv endv)
 
   end:BDD_ABSTRACT_DOMAIN)
 
